73c0238c2834dd7a23a9a686d914b216ca05fe33
ni44ko <nikomatutina@gmail.com>
2018-12-12 20:24:46 +0800

    sample commit from pc

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace DatingApp.API.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "niko", "changed" };
         }
 
         // GET api/values/5
